eb7364f4e6a12091c88f1889d6f5f7038fa6e24c
KencyOseguera <kencyoseguera7@gmail.com>
2025-03-18 18:34:51 -0600

    Reapply "Merge branch 'pruebas' into pruebasSteven"
    This reverts commit 41365880be6d0d4a21d44239e2229a07a388cbfe.

--- login.php
+++ login.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/loginStyle.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <title>Login</title>
-</head>
+
+<?php
+    require_once("./includes/head.php");
+?>
+
 <body>
-                           <!-- Carrousel !-->
+
+  <!-- Carrousel !-->
   <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner">
       <div class="carousel-item active">
@@ -17,73 +16,24 @@
       <div class="carousel-item">
         <img class="d-block w-100" src="assets/img/B2.jpg" alt="Second slide">
       </div>
-      <div class="carousel-item">
-        <img class="d-block w-100" src="assets/img/1847.jpg" alt="Third slide">
-      </div>
-      <div class="carousel-item">
-        <img class="d-block w-100" src="assets/img/K1K2.jpg" alt="Third slide">
-      </div>
-      <div class="carousel-item">
-        <img class="d-block w-100" src="assets/img/C3.jpg" alt="Third slide">
-      </div>
-      <div class="carousel-item">
-        <img class="d-block w-100" src="assets/img/I1.jpg" alt="Third slide">
-      </div>
-      <div class="carousel-item">
-        <img class="d-block w-100" src="assets/img/CentroAcuatico.jpg" alt="Third slide">
-      </div>
-      <div class="carousel-item">
-        <img class="d-block w-100" src="assets/img/F1D1" alt="Third slide">
-      </div>
-      <div class="carousel-item">
-        <img class="d-block w-100" src="assets/img/Biblioteca" alt="Third slide">
-      </div>
-      <div class="carousel-item">
-        <img class="d-block w-100" src="assets/img/Polideportivo" alt="Third slide">
-      </div>
+
     </div>
   </div>
 </body>
 
-  <div class="modal fade show" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="false" style="display:block;">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalCenterTitle">Inicio de sesion</h5>
-        </div>
-        <div class="modal-body">
-          <div class="loginInfo">
-            <form>
-              <div class="form-group">
-                  <label for="exampleFormControlSelect1">Estas iniciando sesion como:</label>
-                  <select class="form-control" id="exampleFormControlSelect1">
-                    <option>Estudiante</option>
-                    <option>Estudiante Post-Grado</option>
-                    <option>Docente</option>
-                    <option>Jefe de Departamento</option>
-                    <option>Coordinador</option>
-                  </select>
-              </div>
-              <div class="form-group">
-                    <label for="exampleInputEmail1">Correo Electronico</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-              </div>
-              <div class="form-group">
-                    <label for="exampleInputPassword1">Contraseña</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-              </div>
-            </div>
-          </div>
-          <div class="modal-footer">
-          <button type="submit" class="btn btn-primary">Enviar</button>
-          </form>
-        </div>
-      </div>
-    </div>
-  </div>
+  
+  <!--Formulario -->
+  
 
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+
+  <?php
+    require_once("./includes/scripts.php");
+  ?>
+    
+ 
+
 </body>
+
 </html>
